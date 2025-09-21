a31ea4152ba081a5431527ca45754677dc660770
Nadeem_Ahmed <nadeemahmed9480@gmail.com>
2025-09-21 12:12:54 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS enable with multiple origins
   app.enableCors({
-    origin: 'https://edviron-software-dev-git-db1f4d-nadeem-ahmeds-projects-a061e572.vercel.app',
-    'https://edviron-software-developer-assessment-dvs9-245k1jir6.vercel.app', 
+    origin: [
+      'http://localhost:5173', // dev
+      'https://edviron-software-dev-git-db1f4d-nadeem-ahmeds-projects-a061e572.vercel.app',
+      'https://edviron-software-developer-assessment-dvs9-245k1jir6.vercel.app' 
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    credentials: true
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
