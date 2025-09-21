47035620cad69e3f133d393fa09b5d3d0334b759
Nadeem_Ahmed <nadeemahmed9480@gmail.com>
2025-09-21 11:50:11 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // frontend URL
+    origin: 'edviron-software-dev-git-db1f4d-nadeem-ahmeds-projects-a061e572.vercel.app', // frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
